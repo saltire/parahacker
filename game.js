4f69364bd9249800517af838c5212db9b818f7a1
saltire sable <saltiresable@gmail.com>
2016-01-03 16:14:58 -0500

    Light up center lights

--- game.js
+++ game.js
@@ -2,40 +2,106 @@ var canvas = document.getElementById('game');
 canvas.width = 800;
 canvas.height = 600;
 canvas.addEventListener('click', onClick, false);
-document.addEventListener('keydown', onKey, false);
 
 var c = canvas.getContext('2d');
+// set origin point at centre top
+c.translate(canvas.width / 2, 0);
 
 
-// Generation data
+// Game object
 
-var wireTypes = [
-    {
-        rows: 1,
-        draw: function (t) {
-            c.beginPath();
-            c.moveTo(-360, t + 20);
-            c.lineTo(-40, t + 20);
-            c.stroke();
-        }
-    },
-    {
-        rows: 2,
-        draw: function (t) {
-            c.beginPath();
-            c.moveTo(-360, t + 20);
-            c.lineTo(-200, t + 20);
-            c.lineTo(-200, t + 60);
-            c.lineTo(-40, t + 60);
-            c.stroke();
-        }
+var Game = function () {
+    // draw background
+    c.fillStyle = '#000';
+    c.fillRect(-canvas.width / 2, 0, canvas.width, canvas.height);
+    c.fillStyle = '#666';
+    c.fillRect(-canvas.width / 2 + 10, 10, canvas.width - 20, canvas.height - 20);
+
+    // set default line properties
+    c.lineWidth = 4;
+    c.lineCap = 'square';
+    c.lineJoin = 'bevel';
+    c.strokeStyle = '#000';
+
+    // draw player sides
+    this.players = [
+        new Player(this, {
+            side: 0,
+            color: '#fbfb00',
+            nodes: 3
+        }),
+        new Player(this, {
+            side: 1,
+            color: '#e600e6',
+            nodes: 5
+        })
+    ];
+
+    // draw row lights
+    this.rowLights = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+    for (var i = 0; i < 12; i++) {
+        this.drawRowLight(i);
     }
-];
+
+    // draw top light
+    this.drawTopLight();
+};
+
+Game.prototype.drawTopLight = function () {
+    var counts = [0, 0];
+    this.rowLights.forEach(function (val) {
+        if (val !== null) {
+            counts[val] += 1;
+        }
+    });
+
+    if (counts[0] > counts[1]) {
+        c.fillStyle = this.players[0].color;
+    }
+    else if (counts[0] < counts[1]) {
+        c.fillStyle = this.players[1].color;
+    }
+    else {
+        c.fillStyle = '#000';
+    }
+
+    c.fillRect(-40, 20, 80, 80);
+    c.strokeRect(-40, 20, 80, 80);
+};
+
+Game.prototype.drawRowLight = function (row) {
+    var t = 100 + row * 40;
+    c.fillStyle = this.rowLights[row] !== null ? this.players[this.rowLights[row]].color : '#666';
+    c.fillRect(-40, t, 80, 40);
+    c.strokeRect(-40, t, 80, 40);
+};
+
+Game.prototype.setRowLight = function (row, player) {
+    this.rowLights[row] = this.players.indexOf(player);
+    this.drawRowLight(row);
+
+    this.drawTopLight();
+};
+
+Game.prototype.onClick = function (x, y) {
+    var row = Math.floor((y - 100) / 40);
+    if (row < 0 || row >= 12) {
+        return;
+    }
+
+    if (x >= 40 && x <= 360) {
+        this.players[0].onRowSelect(row);
+    }
+    else if (x >= 440 && x <= 760) {
+        this.players[1].onRowSelect(row);
+    }
+};
 
 
 // Player object
 
-var Player = function (opts) {
+var Player = function (game, opts) {
+    this.game = game;
     this.side = opts.side;
     this.color = opts.color;
     this.nodes = opts.nodes;
@@ -66,7 +132,7 @@ Player.prototype.drawSide = function () {
         var wireType = wireTypes[Math.floor(Math.random() * wireTypes.length)];
         if (row + wireType.rows <= 12) {
             this.wires.push({
-                top: 100 + row * 40,
+                row: row,
                 hasNode: false,
                 type: wireType
             });
@@ -106,11 +172,13 @@ Player.prototype.drawNode = function (x, y) {
 };
 
 Player.prototype.drawWire = function (wire) {
+    var t = 100 + wire.row * 40;
     c.strokeStyle = wire.hasNode ? this.color : '#000';
-    wire.type.draw(wire.top);
+
+    wire.type.draw(t);
     c.strokeStyle = '#000';
     if (wire.hasNode) {
-        this.drawNode(-340, wire.top + 20);
+        this.drawNode(-340, t + wire.type.startRow * 40 + 20);
     }
 };
 
@@ -131,44 +199,45 @@ Player.prototype.onRowSelect = function (row) {
 
     c.fillStyle = this.color;
     this.drawWire(wire);
+
+    var endRow = wire.row + wire.type.endRow;
+    this.game.setRowLight(endRow, this);
 };
+
+
+// Generation data
+
+var wireTypes = [
+    {
+        rows: 1,
+        startRow: 0,
+        endRow: 0,
+        draw: function (t) {
+            c.beginPath();
+            c.moveTo(-360, t + 20);
+            c.lineTo(-40, t + 20);
+            c.stroke();
+        }
+    },
+    {
+        rows: 2,
+        startRow: 0,
+        endRow: 1,
+        draw: function (t) {
+            c.beginPath();
+            c.moveTo(-360, t + 20);
+            c.lineTo(-200, t + 20);
+            c.lineTo(-200, t + 60);
+            c.lineTo(-40, t + 60);
+            c.stroke();
+        }
+    }
+];
 
 
 // Initialize
 
-// draw background
-c.fillStyle = '#000';
-c.fillRect(0, 0, canvas.width, canvas.height);
-c.fillStyle = '#666';
-c.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
-
-// set default line properties
-c.lineWidth = 4;
-c.lineCap = 'square';
-c.strokeStyle = '#000';
-
-// set origin point at centre top
-c.translate(canvas.width / 2, 0);
-
-// draw top light
-c.strokeRect(-40, 20, 80, 80);
-
-// draw centre lights
-for (var t = 100; t <= 540; t += 40) {
-    c.strokeRect(-40, t, 80, 40);
-}
-
-// draw player sides
-var player1 = new Player({
-    side: 0,
-    color: '#ffff00',
-    nodes: 3
-});
-var player2 = new Player({
-    side: 1,
-    color: '#ff00ff',
-    nodes: 5
-});
+var game = new Game();
 
 
 // Event handlers
@@ -177,19 +246,5 @@ function onClick(e) {
     var x = e.offsetX;
     var y = e.offsetY;
 
-    var row = Math.floor((y - 100) / 40);
-    if (row < 0 || row >= 12) {
-        return;
-    }
-
-    if (x >= 40 && x <= 360) {
-        player1.onRowSelect(row);
-    }
-    else if (x >= 440 && x <= 760) {
-        player2.onRowSelect(row);
-    }
-}
-
-function onKey(e) {
-    console.log(e);
+    game.onClick(x, y);
 }
